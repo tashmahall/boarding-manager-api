f5ace780cfe5440920e781be8548598e59bd4ae6
tashm <tashm@10.19.83.151>
2021-03-18 20:52:31 -0300

    create do eclipse project and create the contract services

---
+++ src/main/java/com/igor/boardingmanager/BoardingManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.igor.boardingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igor/boardingmanager/BoardingManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.boardingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
