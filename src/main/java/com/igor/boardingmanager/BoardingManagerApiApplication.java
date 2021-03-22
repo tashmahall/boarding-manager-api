eb053134eea631f29d623c336aaf8e317f447109
tashm <tashm@DESKTOP-P186CUG.mshome.net>
2021-03-22 00:48:53 -0300

    configure openapi to work

--- src/main/java/com/igor/boardingmanager/BoardingManagerApiApplication.java
+++ src/main/java/com/igor/boardingmanager/BoardingManagerApiApplication.java
@@ -1,13 +1,25 @@
 package com.igor.boardingmanager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class BoardingManagerApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardingManagerApiApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Store API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
