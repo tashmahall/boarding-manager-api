59c82757255c75a5cc6324cde74a7cdd72cdcc4e
tashm <tashm@DESKTOP-P186CUG.mshome.net>
2021-03-22 13:42:40 -0300

    Update the attributes constraints

--- src/main/java/com/igor/boardingmanager/controller/mappers/dtos/EmployeeDTO.java
+++ src/main/java/com/igor/boardingmanager/controller/mappers/dtos/EmployeeDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.lang.NonNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
@@ -19,12 +20,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EmployeeDTO extends RepresentationModel<EmployeeDTO> { 
 	@CPF(message="Invalid CPF")
+	@NonNull
 	private String cpf;
-	
+	@NonNull
 	private String name;
-	
+	@NonNull
 	private String companyName;
-	
+	@NonNull
 	private String functionName;
 	
 	private LocalDate boardingDate;
